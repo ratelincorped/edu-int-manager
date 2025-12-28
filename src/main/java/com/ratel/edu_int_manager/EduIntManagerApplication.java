559d0172a83e0288cf4f2ba852fe928da86ca373
Francis Kangaty <franciskangaty@gmail.com>
2025-12-28 18:42:55 +0300

    first commit

---
+++ src/main/java/com/ratel/edu_int_manager/EduIntManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ratel.edu_int_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduIntManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduIntManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratel/edu_int_manager/EduIntManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratel.edu_int_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduIntManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
